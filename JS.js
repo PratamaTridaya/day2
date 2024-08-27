34fced7212a2499747ceb57c80577fdaf757b319
Pratama <mpratamary@gmail.com>
2024-08-27 14:52:55 +0700

    update day2

---
+++ JS.js
@@ -0,0 +1,20 @@
+//1.
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("demo").innerHTML = myFunction(4, 3);
+
+//2.
+let variabel;
+variabel = "nilai";
+variabel = 0;
+variabel = 5.6;
+variabel = true;
+variabel = "data";
+
+console.log(variabel);
+document.getElementById("variableValue").innerHTML = variabel;
+
+//3.
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo2").innerHTML = fruits;
